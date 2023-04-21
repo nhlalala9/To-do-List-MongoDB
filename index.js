ce74835a33c0a2e16fcf95ed08d9c516c0f6f823
Nhlalala <nhlalala60@gmail.com>
2023-04-21 12:27:49 +0200

    server

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const app = express();
 
 app.use("/static",express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.render('todo.ejs');
 });
 
+// POST Method
 
 
-app.listen(3000, () => console.log("Server Up and running"));
+app.post('/',(req, res) => {
+    console.log(req.body);
+    });
+
+app.listen(3000, () => console.log("Server is running without error"));
