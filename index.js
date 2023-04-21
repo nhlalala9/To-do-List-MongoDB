cf6821ccfc89b5d17379b797e44557948de848c1
Nhlalala <nhlalala60@gmail.com>
2023-04-21 12:18:49 +0200

    todo.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use("/static",express.static("public"));
 
 app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('todo.ejs');
+});
+
+
 
 app.listen(3000, () => console.log("Server Up and running"));
