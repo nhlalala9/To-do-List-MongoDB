c1ab9d99c093d08fb4789b30b34e9810b20706be
Nhlalala <nhlalala60@gmail.com>
2023-04-21 15:57:14 +0200

    server

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+dotenv.config();
 
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
+
+//connection to db
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+console.log("Connected to db!");
+
+app.listen(3000, () => console.log("Server is running without error"));
+
+})
+
 app.set("view engine", "ejs");
+
+// GET Method
 
 app.get('/', (req, res) => {
     res.render('todo.ejs');
@@ -16,5 +34,3 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req.body);
 });
-
-app.listen(3000, () => console.log("Server is running without error"));
