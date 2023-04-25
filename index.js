fda7a2534ca3eb1764631e549eea4c2784b91d99
Nhlalala <nhlalala60@gmail.com>
2023-04-25 13:56:31 +0200

    changes pass

--- index.js
+++ index.js
@@ -13,14 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //connection to db
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-console.log("Connected to db!");
+
+// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+// console.log("Connected to db!");
 
 app.listen(3000, () => console.log("Server is running without error"));
 
-})
+// })
 
 app.set("view engine", "ejs");
 
